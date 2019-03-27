65617976e068d20eb7fdb43c550249e8ffb12acf
Jos3L <jos.street@gmail.com>
2019-03-27 19:02:19 -0400

    child

--- Child.java
+++ Child.java
@@ -1,3 +1,10 @@
 public class Child{
-
+   public static final MIN_AGE = 9;
+   public static final MAX_AGE = 16;
+   public static final MAX_CHILDREN = 17;
+   public static final BASE_RATE = 79;
+   public static final TRANSPORTATION_FEE = 23;
+   public static final MIN_COURSES = 1;
+   public static final MAX_COURSES = 6;
+   
 }
